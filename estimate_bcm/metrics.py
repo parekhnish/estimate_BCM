1fe3bc07eebb4ab2ecd827b0952d55a031935949
Nishit Parekh <parekhnish@gmail.com>
2022-04-03 01:31:43 -0400

    Metrics: Added lots of stuff, including lower/upper limits and uncertainty ranges

--- estimate_bcm/metrics.py
+++ estimate_bcm/metrics.py
@@ -1,12 +1,33 @@
+from typing import Union, Tuple
+from decimal import Decimal
+
+import numpy as np
 from sympy import Symbol
 
 
 class Metric:
-    symb_name = "placeholder_metric"
+    symb_name = ""      # These are dummy placeholders! Replace with actual values in the metric implementations!
+    lower_lim = 0.0     # ' ' '
+    upper_lim = 1.0     # ' ' '
 
-    def __init__(self, supplied_value):
+    def __init__(self, supplied_value: Decimal,
+                 uncertainty_range_param: Union[str, float, tuple, None] = "round"):
+
         self._symbol = Symbol(self.symb_name)
+
         self.supplied_value = supplied_value
+        self.current_value = float(self.supplied_value)
+
+        self.uncertainty_range_param = uncertainty_range_param
+        self.uncertainty_range = self._compute_uncertainty_range(
+            uncertainty_range_param=self.uncertainty_range_param,
+            current_value=self.current_value,
+            supplied_value=self.supplied_value
+        )
+
+        self.uncertainty_range[0] = max(self.uncertainty_range[0], self.lower_lim)
+        self.uncertainty_range[1] = max(self.uncertainty_range[1], self.upper_lim)
+
 
     @property
     def symbol(self):
@@ -17,11 +38,86 @@ class Metric:
         raise NotImplementedError
 
 
-class LinearMetric(Metric):
-    symb_name = "placeholder_linear_metric"
+    @staticmethod
+    def _compute_uncertainty_range(uncertainty_range_param: Union[str, float, tuple, None],
+                                   current_value: float,
+                                   supplied_value: Decimal):
 
-    def __init__(self, supplied_value):
-        super().__init__(supplied_value)
+        if uncertainty_range_param is None:
+            uncertainty_range = (current_value, current_value)
+
+        elif isinstance(uncertainty_range_param, str):
+            if uncertainty_range_param.endswith("%"):
+                uncertainty_range = Metric._compute_metric_uncertainty_range_from_tuple(
+                    t=(uncertainty_range_param, uncertainty_range_param),
+                    orig_val=current_value
+                )
+
+            else:
+                prec_exp = supplied_value.as_tuple().exponent
+                prec_step = float(f"1e{prec_exp}")
+
+                if uncertainty_range_param == "round":
+                    uncertainty_range = (current_value - (prec_step/2.0), current_value + (prec_step/2.0))
+                elif uncertainty_range_param == "floor":
+                    uncertainty_range = (current_value - prec_step, current_value)
+                elif uncertainty_range_param == "ceil":
+                    uncertainty_range = (current_value, current_value + prec_step)
+                else:
+                    raise ValueError(f"{uncertainty_range_param} is not a valid string for uncertainty_range_param; Expected one of 'round', 'floor', 'ceil'")
+
+
+        elif isinstance(uncertainty_range_param, (float, int)):
+            uncertainty_range = Metric._compute_metric_uncertainty_range_from_tuple(
+                t=(uncertainty_range_param, uncertainty_range_param),
+                orig_val=current_value
+            )
+
+        elif isinstance(uncertainty_range_param, tuple):
+
+            if len(uncertainty_range_param) != 2:
+                raise SyntaxError(f"{uncertainty_range_param} is not a valid tuple for uncertainty_range_param; Expected a tuple of length 2")
+
+            uncertainty_range = Metric._compute_metric_uncertainty_range_from_tuple(
+                t=uncertainty_range_param,
+                orig_val=current_value
+            )
+
+        else:
+            raise SyntaxError(f"{uncertainty_range_param} (type: {type(uncertainty_range_param)}) is not a valid uncertainty_range_param; Expected a str, float, tuple, or None")
+
+        return uncertainty_range
+
+
+    @staticmethod
+    def _compute_metric_uncertainty_range_from_tuple(t: Tuple[Union[str, float], Union[str, float]],
+                                                     orig_val: float):
+
+        diff_tuple = (None, None)
+        for i in range(2):
+
+            if isinstance(t[i], str):
+                if not t[i].endswith("%"):
+                    raise ValueError(f"Element {i} of uncertainty_range_param tuple {t} must be a string ending with %")
+
+                ratio = float(t[i][:-1]) / 100.0
+                diff_tuple[i] = orig_val * ratio
+
+            else:
+                diff_tuple[i] = t[i]
+
+        uncertainty_range = (orig_val - diff_tuple[0], orig_val + diff_tuple[1])
+        return uncertainty_range
+
+
+class LinearMetric(Metric):
+
+    def __init__(self, supplied_value: Decimal,
+                 perturb_during_estimation=True,
+                 **metric_kwargs):
+
+        super().__init__(supplied_value, **metric_kwargs)
+        self.perturb_during_estimation = perturb_during_estimation
 
     @staticmethod
     def get_eqn(v):
@@ -31,14 +127,16 @@ class LinearMetric(Metric):
         return self.get_eqn_for_linear_system(self.symbol)
 
     def get_numeric_eqn(self):
-        return self.get_eqn(self.supplied_value)
+        return self.get_eqn(self.current_value)
 
 
 class Accuracy(LinearMetric):
     symb_name = "acc"
+    lower_lim = 0.0
+    upper_lim = 1.0
 
-    def __init__(self, supplied_value):
-        super().__init__(supplied_value)
+    def __init__(self, supplied_value: Decimal, **kwargs):
+        super().__init__(supplied_value, **kwargs)
 
     @staticmethod
     def get_eqn(v):
@@ -51,9 +149,11 @@ class Accuracy(LinearMetric):
 
 class F1_Score(LinearMetric):
     symb_name = "f1"
+    lower_lim = 0.0
+    upper_lim = 1.0
 
-    def __init__(self, supplied_value):
-        super().__init__(supplied_value)
+    def __init__(self, supplied_value: Decimal, **kwargs):
+        super().__init__(supplied_value, **kwargs)
 
     @staticmethod
     def get_eqn(v):
@@ -66,9 +166,11 @@ class F1_Score(LinearMetric):
 
 class PPV(LinearMetric):
     symb_name = "ppv"
+    lower_lim = 0.0
+    upper_lim = 1.0
 
-    def __init__(self, supplied_value):
-        super().__init__(supplied_value)
+    def __init__(self, supplied_value: Decimal, **kwargs):
+        super().__init__(supplied_value, **kwargs)
 
     @staticmethod
     def get_eqn(v):
@@ -81,9 +183,11 @@ class PPV(LinearMetric):
 
 class ActualPositive(LinearMetric):
     symb_name = "actual_p"
+    lower_lim = 0
+    upper_lim = np.inf
 
-    def __init__(self, supplied_value):
-        super().__init__(supplied_value)
+    def __init__(self, supplied_value: Decimal, **kwargs):
+        super().__init__(supplied_value, **kwargs)
 
     @staticmethod
     def get_eqn(v):
@@ -96,9 +200,11 @@ class ActualPositive(LinearMetric):
 
 class ActualNegative(LinearMetric):
     symb_name = "actual_n"
+    lower_lim = 0
+    upper_lim = np.inf
 
-    def __init__(self, supplied_value):
-        super().__init__(supplied_value)
+    def __init__(self, supplied_value: Decimal, **kwargs):
+        super().__init__(supplied_value, **kwargs)
 
     @staticmethod
     def get_eqn(v):
@@ -111,9 +217,11 @@ class ActualNegative(LinearMetric):
 
 class PredictedPositive(LinearMetric):
     symb_name = "pred_p"
+    lower_lim = 0
+    upper_lim = np.inf
 
-    def __init__(self, supplied_value):
-        super().__init__(supplied_value)
+    def __init__(self, supplied_value: Decimal, **kwargs):
+        super().__init__(supplied_value, **kwargs)
 
     @staticmethod
     def get_eqn(v):
@@ -126,9 +234,11 @@ class PredictedPositive(LinearMetric):
 
 class PredictedNegative(LinearMetric):
     symb_name = "pred_n"
+    lower_lim = 0
+    upper_lim = np.inf
 
-    def __init__(self, supplied_value):
-        super().__init__(supplied_value)
+    def __init__(self, supplied_value: Decimal, **kwargs):
+        super().__init__(supplied_value, **kwargs)
 
     @staticmethod
     def get_eqn(v):
